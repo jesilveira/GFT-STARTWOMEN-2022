be28943923a6d894c7f0adfa38bffc1092fd9add
Jéssika Santos Silveira <72706260+jesilveira@users.noreply.github.com>
2022-06-22 11:23:37 -0300

    Update Aniversário.java

--- src/Exercicio4/Aniversário.java
+++ src/Exercicio4/Aniversário.java
@@ -3,7 +3,7 @@ package Exercicio4;
 import java.util.Locale;
 import java.util.Scanner;
 
-public class Aniversário {
+public class AniversÃ¡rio {
 
 	public static void main(String[] args) {
 
@@ -13,21 +13,21 @@ public class Aniversário {
 		int dia, mes;
 		double ano;
 
-		System.out.println("Digite o dia do seu aniversário: ");
+		System.out.println("Digite o dia do seu aniversÃ¡rio: ");
 		dia = sc.nextInt();
 
-		System.out.println("Digite o mês de Nascimento");
+		System.out.println("Digite o mÃªs de Nascimento");
 		mes = sc.nextInt();
 
 		System.out.println("Digite o ano de Nascimento");
 		ano = sc.nextDouble();
 
 		if (mes > ano) {
-			System.out.println("Data Inválida");
+			System.out.println("Data InvÃ¡lida");
 		}
 
 		else {
-			System.out.println("Data Válida, você digitou mês ou dia inválido");
+			System.out.println("VocÃª digitou mÃªs ou dia invÃ¡lido");
 		}
 		sc.close();
 	} 
